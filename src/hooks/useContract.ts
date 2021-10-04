ce77fe9aaf9700b6c37ab658e59aa2d2b0a4ceb1
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-04 08:17:58 -0300

    Create the interactWithContract helper function in useContract

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -7,30 +7,29 @@ export const useContract = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   const [isMining, setIsMining] = useState(false);
 
-  const checkIfWalletIsConnected = useCallback(async () => {
-    const { ethereum } = window;
+  const interactWithContract = (
+    callback: (contract: ethers.Contract) => void
+  ) => {
+    try {
+      const { ethereum } = window;
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEneftee.abi,
+          signer
+        );
+
+        callback(connectedContract);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account:", account);
-      setCurrentAccount(account);
-
-      // Setup listener! This is for the case where a user comes to our site
-      // and ALREADY had their wallet connected + authorized.
-      setupEventListener();
-    } else {
-      console.log("No authorized account found");
-    }
-  }, []);
+  };
 
   const connectWallet = async () => {
     try {
@@ -48,8 +47,6 @@ export const useContract = () => {
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
 
-      // Setup listener! This is for the case where a user comes to our site
-      // and connected their wallet for the first time.
       setupEventListener();
 
       if (window.ethereum.networkVersion !== "4")
@@ -61,76 +58,62 @@ export const useContract = () => {
     }
   };
 
-  // Setup our listener.
-  const setupEventListener = async () => {
-    // Most of this looks the same as our function askContractToMintNft
-    try {
-      const { ethereum } = window;
+  const setupEventListener = useCallback(async () => {
+    interactWithContract((contract) => {
+      contract.on("NewEpicNFTMinted", (from, tokenId) => {
+        console.log(from, tokenId.toNumber());
+        alert(`Hey there!
+      
+      We've minted your NFT and sent it to your wallet.
+      
+      It may be blank right now. It can take a max of 10 min to show up on OpenSea.
+      
+      Here's the link: 
+      
+      https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+      });
 
-      if (ethereum) {
-        // Same stuff again
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(
-          CONTRACT_ADDRESS,
-          myEneftee.abi,
-          signer
-        );
+      console.log("Setup event listener!");
+    });
+  }, []);
 
-        // This will essentially "capture" our event when our contract throws it.
-        // If you're familiar with webhooks, it's very similar to that!
-        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-          console.log(from, tokenId.toNumber());
-          alert(`Hey there!
-          
-          We've minted your NFT and sent it to your wallet.
-          
-          It may be blank right now. It can take a max of 10 min to show up on OpenSea.
-          
-          Here's the link: 
-          
-          https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
-        });
+  const checkIfWalletIsConnected = useCallback(async () => {
+    const { ethereum } = window;
 
-        console.log("Setup event listener!");
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
-    } catch (error) {
-      console.log(error);
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
     }
-  };
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+
+      setupEventListener();
+    } else {
+      console.log("No authorized account found");
+    }
+  }, [setupEventListener]);
 
   const askContractToMintNft = async () => {
-    try {
-      const { ethereum } = window;
+    interactWithContract(async (contract) => {
+      console.log("Going to pop wallet now to pay gas...");
+      let nftTxn = await contract.makeAnEpicNFT();
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(
-          CONTRACT_ADDRESS,
-          myEneftee.abi,
-          signer
-        );
-
-        console.log("Going to pop wallet now to pay gas...");
-        let nftTxn = await connectedContract.makeAnEpicNFT();
-
-        setIsMining(true);
-        console.log("Mining...please wait.");
-        await nftTxn.wait();
-        console.log(nftTxn);
-        console.log(
-          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
-        );
-        setIsMining(false);
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
-    } catch (error) {
-      console.log(error);
-    }
+      setIsMining(true);
+      console.log("Mining...please wait.");
+      await nftTxn.wait();
+      console.log(nftTxn);
+      console.log(
+        `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+      );
+      setIsMining(false);
+    });
   };
 
   useEffect(() => {
